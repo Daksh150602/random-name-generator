c7778e71b6516dbc407569f6ca2808903dfe18dd
Daksh150602 <107460197+Daksh150602@users.noreply.github.com>
2024-11-21 16:53:54 +0530

    Add files via upload

---
+++ random_name_geenrator.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name geenrator")
+ciy_name=input("What's the name of the city you grew up in?\n")
+pet_name=input("What's your pet's name?\n")
+print("your band name could be "+ciy_name+" "+pet_name)
